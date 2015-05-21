66b6971e430032de6d0bf482434cceec6b8f5355
latitudecode <sambakersalmon@gmail.com>
2015-05-20 23:12:45 -0400

    adding styling and adding single, page list and entries

---
+++ wordpress/wp-content/themes/aleks-theme/page-list.php
@@ -0,0 +1,277 @@
+<body>
+
+<!-- back btn -->
+		<div class="back">
+			<a href="index.html">back to home</a>
+		</div>
+<!-- end back btn -->
+
+<div class="wrapper">
+
+	<header>
+
+	</header>
+
+<!-- main content -->
+	<div class="main-content page-list">
+		<div class="row"> <!-- start row -->
+			<div class="col-md-12">
+				<h1>Lessons</h1>
+			</div>
+		</div> <!-- end row -->
+		<div class="row"> <!-- start row -->
+			<div class="col-md-4"> <!-- start three column -->
+				<ul> <!-- start post list -->
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>																										
+				</ul> <!-- end post list -->
+			</div>
+			<div class="col-md-4">
+<ul> <!-- start post list -->
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>																										
+				</ul> <!-- end post list -->		
+			</div>
+			<div class="col-md-4"> <!-- end three column -->
+<ul> <!-- start post list -->
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>																										
+				</ul> <!-- end post list -->			
+			</div>						
+		</div> <!-- end row -->
+	</div>
+<!-- end main content -->
---
+++ wordpress/wp-content/themes/aleks-theme/single.php
@@ -0,0 +1,56 @@
+<body>
+
+<!-- back btn -->
+		<div class="back">
+			<a href="page-list.html">back to lessons</a>
+		</div>
+<!-- end back btn -->
+
+<div class="wrapper">
+
+	<header>
+
+	</header>
+
+<!-- main content -->
+	<div class="main-content">
+		<div class="row post-content"> <!-- start row -->
+			<div class="col-md-12"> <!-- start three column -->
+				<h1>Title of lesson</h1>
+			</div>						
+		</div> <!-- end row -->
+		<div class="row post-content"> <!-- start row -->
+			<div class="col-md-12"> <!-- start three column -->
+				<h2>sub title</h2>
+				<p>Pinterest High Life authentic Blue Bottle squid. Fixie hella tousled, beard organic scenester pour-over Blue Bottle seitan trust fund twee fap. Pork belly butcher seitan cray 3 wolf moon. Actually selfies YOLO PBR&B tousled PBR. Cray Portland banjo, mlkshk Wes Anderson four loko flannel distillery twee tilde bicycle rights small batch. Gastropub church-key narwhal four dollar toast leggings next level. Hoodie raw denim meditation you probably haven't heard of them letterpress.
+				<br />
+				<br />
+				Vinyl vegan sartorial aesthetic bitters hella. <a href="#">Neutra bitters</a> tousled locavore, hashtag trust fund bespoke Blue Bottle. Four dollar toast sriracha organic, twee hella Tumblr flannel keffiyeh beard polaroid cray put a bird on it. Swag four dollar toast cred pop-up. Occupy Pitchfork letterpress master cleanse, sartorial hoodie taxidermy Brooklyn salvia hashtag. McSweeney's ethical art party biodiesel fixie. Pitchfork Kickstarter leggings aesthetic tattooed Godard, kitsch keffiyeh McSweeney's lomo listicle fixie flexitarian High Life ennui.
+				<br />
+				<br />
+				<img src="images/image.png" />
+				<br />
+				<br />
+				Mlkshk pickled tousled hoodie. Pork belly mustache ethical tofu Wes Anderson cred. Kale chips wolf drinking vinegar umami Carles, vegan chillwave beard shabby chic Echo Park you probably haven't heard of them craft beer. Readymade Neutra raw denim dreamcatcher fap, migas craft beer master cleanse. Portland slow-carb skateboard biodiesel cred next level, four loko mumblecore. Cred ennui Pinterest viral, disrupt Tumblr High Life cronut jean shorts 3 wolf moon polaroid cornhole pop-up wayfarers. Fap Intelligentsia cred Pitchfork American Apparel cray, church-key whatever.
+				<br />
+				<br />
+				Vinyl vegan sartorial <a href="#">aesthetic bitters</a> hella. Neutra bitters tousled locavore, hashtag trust fund bespoke Blue Bottle. Four dollar toast sriracha organic, twee hella Tumblr flannel keffiyeh beard polaroid cray put a bird on it. Swag four dollar toast cred pop-up. Occupy Pitchfork letterpress master cleanse, sartorial hoodie taxidermy Brooklyn salvia hashtag. McSweeney's ethical art party biodiesel fixie. Pitchfork Kickstarter leggings aesthetic tattooed Godard, kitsch keffiyeh McSweeney's lomo listicle fixie flexitarian High Life ennui.
+				<p>Pinterest High Life authentic Blue Bottle squid. Fixie hella tousled, beard organic scenester pour-over Blue Bottle seitan trust fund twee fap. Pork belly butcher seitan cray 3 wolf moon. Actually selfies YOLO PBR&B tousled PBR. Cray Portland banjo, mlkshk Wes Anderson four loko flannel distillery twee tilde bicycle rights small batch. Gastropub church-key narwhal four dollar toast leggings next level. Hoodie raw denim meditation you probably haven't heard of them letterpress.
+				<br />
+				<br />
+				Vinyl vegan sartorial aesthetic bitters hella. <a href="#">Neutra bitters</a> tousled locavore, hashtag trust fund bespoke Blue Bottle. Four dollar toast sriracha organic, twee hella Tumblr flannel keffiyeh beard polaroid cray put a bird on it. Swag four dollar toast cred pop-up. Occupy Pitchfork letterpress master cleanse, sartorial hoodie taxidermy Brooklyn salvia hashtag. McSweeney's ethical art party biodiesel fixie. Pitchfork Kickstarter leggings aesthetic tattooed Godard, kitsch keffiyeh McSweeney's lomo listicle fixie flexitarian High Life ennui.
+				<br />
+				<br />
+				<img src="images/image.png" />
+				<br />
+				<br />
+				Mlkshk pickled tousled hoodie. Pork belly mustache ethical tofu Wes Anderson cred. Kale chips wolf drinking vinegar umami Carles, vegan chillwave beard shabby chic Echo Park you probably haven't heard of them craft beer. Readymade Neutra raw denim dreamcatcher fap, migas craft beer master cleanse. Portland slow-carb skateboard biodiesel cred next level, four loko mumblecore. Cred ennui Pinterest viral, disrupt Tumblr High Life cronut jean shorts 3 wolf moon polaroid cornhole pop-up wayfarers. Fap Intelligentsia cred Pitchfork American Apparel cray, church-key whatever.
+				<br />
+				<br />
+				Vinyl vegan sartorial <a href="#">aesthetic bitters</a> hella. Neutra bitters tousled locavore, hashtag trust fund bespoke Blue Bottle. Four dollar toast sriracha organic, twee hella Tumblr flannel keffiyeh beard polaroid cray put a bird on it. Swag four dollar toast cred pop-up. Occupy Pitchfork letterpress master cleanse, sartorial hoodie taxidermy Brooklyn salvia hashtag. McSweeney's ethical art party biodiesel fixie. Pitchfork Kickstarter leggings aesthetic tattooed Godard, kitsch keffiyeh McSweeney's lomo listicle fixie flexitarian High Life ennui.
+								
+				</p>
+			</div>						
+		</div> <!-- end row -->		
+	</div>
+<!-- end main content -->
