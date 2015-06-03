9bf8b34dc5ab5457c84e15dbd42f52cef093c277
latitudecode <sambakersalmon@gmail.com>
2015-06-02 21:32:39 -0400

    adding all page loops to homepage

--- wordpress/wp-content/themes/aleks-theme/index.php
+++ wordpress/wp-content/themes/aleks-theme/index.php
@@ -3,10 +3,17 @@
 <!-- main content -->
 	<div class="main-content">
 		<div class="main-entries row"> <!-- start row -->
+
+			<!-- start page loop -->
+			<?php query_posts('post_type=page&p=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
+
 			<div class="col-md-4 studies"> <!-- start three column -->
 				<h2>Studies</h2>
 				<p>coming soon</p>
 			</div>
+
+			<?php endwhile; endif; wp_reset_query(); ?>	
+			<!-- end page loop -->
 
 			<!-- start page loop -->
 			<?php query_posts('post_type=page&p=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
@@ -19,10 +26,17 @@
 			<?php endwhile; endif; wp_reset_query(); ?>	
 			<!-- end page loop -->
 
+			<!-- start page loop -->
+			<?php query_posts('post_type=page&p=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
+
 			<div class="col-md-4 experiments"> <!-- end three column -->
 				<h2>Experiments</h2>
 				<p>coming soon</p>			
-			</div>						
+			</div>	
+
+			<?php endwhile; endif; wp_reset_query(); ?>	
+			<!-- end page loop -->
+
 		</div> <!-- end row -->
 	</div>
 <!-- end main content -->
