e42d12fc5dd6f4864756cc3bae0c3c1cdf14aaa7
sphrcl <sphericalcommunications@gmail.com>
2015-06-02 21:30:20 -0400

    loop page ID on homepage

--- wordpress/wp-content/themes/aleks-theme/index.php
+++ wordpress/wp-content/themes/aleks-theme/index.php
@@ -7,10 +7,18 @@
 				<h2>Studies</h2>
 				<p>coming soon</p>
 			</div>
+
+			<!-- start page loop -->
+			<?php query_posts('post_type=page&p=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
+
 			<div class="col-md-4 lessons">
 				<h2>Lessons</h2>
-				<a class="btn" href="page-list.html">read more</a>			
+				<a class="btn" href="<?php the_permalink(); ?>">read more</a>			
 			</div>
+
+			<?php endwhile; endif; wp_reset_query(); ?>	
+			<!-- end page loop -->
+
 			<div class="col-md-4 experiments"> <!-- end three column -->
 				<h2>Experiments</h2>
 				<p>coming soon</p>			
@@ -20,5 +28,6 @@
 <!-- end main content -->
 
 <?php wp_footer(); ?>
+
 
 
