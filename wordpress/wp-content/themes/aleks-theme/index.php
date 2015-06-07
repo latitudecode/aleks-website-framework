84db4b5a2b1a90df4fcb31380367df8ad2f53b26
latitudecode <sambakersalmon@gmail.com>
2015-06-07 15:13:48 -0400

    Revert 66b6971..8aba89b
    This rolls back to commit 66b6971e430032de6d0bf482434cceec6b8f5355.

--- wordpress/wp-content/themes/aleks-theme/index.php
+++ wordpress/wp-content/themes/aleks-theme/index.php
@@ -1,49 +1,18 @@
 <!-- main content -->
 	<div class="main-content">
 		<div class="main-entries row"> <!-- start row -->
-
-			<!-- start page loop -->
-			<?php query_posts('post_type=page&p=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
-
 			<div class="col-md-4 studies"> <!-- start three column -->
 				<h2>Studies</h2>
 				<p>coming soon</p>
 			</div>
-
-			<?php endwhile; endif; wp_reset_query(); ?>	
-			<!-- end page loop -->
-
-			<!-- start page loop -->
-			<?php query_posts('post_type=page&p=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
-
 			<div class="col-md-4 lessons">
 				<h2>Lessons</h2>
-				<a class="btn" href="<?php the_permalink(); ?>">read more</a>			
+				<a class="btn" href="page-list.html">read more</a>			
 			</div>
-
-			<?php endwhile; endif; wp_reset_query(); ?>	
-			<!-- end page loop -->
-
-			<!-- start page loop -->
-			<?php query_posts('post_type=page&p=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
-
 			<div class="col-md-4 experiments"> <!-- end three column -->
 				<h2>Experiments</h2>
 				<p>coming soon</p>			
-			</div>	
-
-			<?php endwhile; endif; wp_reset_query(); ?>	
-			<!-- end page loop -->
-
+			</div>						
 		</div> <!-- end row -->
 	</div>
-<<<<<<< HEAD
 <!-- end main content -->
-
-<?php wp_footer(); ?>
-
-
-
-=======
-<!-- end main content -->
->>>>>>> parent of 8f67951... adding header footer and sourcing css js
--- wordpress/wp-content/themes/aleks-theme/page-list.php
+++ wordpress/wp-content/themes/aleks-theme/page-list.php
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<?php
-/*
- * Template Name: Lessons
- * Description: List of posts
- */
-
-//
-
-?>
-
-<?php get_header(); ?>
-
-
-=======
->>>>>>> parent of 8f67951... adding header footer and sourcing css js
 <body>
 
 <!-- back btn -->
@@ -35,29 +19,91 @@
 				<h1>Lessons</h1>
 			</div>
 		</div> <!-- end row -->
-
 		<div class="row"> <!-- start row -->
-			 
-			 <?php query_posts('cat=3'); if(have_posts()) : while(have_posts()) : the_post(); ?> <!-- starting the WordPress category loop -->
-
 			<div class="col-md-4"> <!-- start three column -->
 				<ul> <!-- start post list -->
 					<li>
-						<a class="btn" href="<?php the_permalink(); ?>">
-							<h2><?php the_title(); ?></h2>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
 						</a>
-					</li>																							
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>
+					<li>
+						<a class="btn" href="single.html">
+							<h2>Title of lesson</h2>
+						</a>
+					</li>																										
 				</ul> <!-- end post list -->
 			</div>
-<<<<<<< HEAD
-
-				<?php endwhile; endif; wp_reset_query(); ?>	
-
-		</div>
-<!-- end main content -->
-
-<?php wp_footer(); ?>
-=======
 			<div class="col-md-4">
 <ul> <!-- start post list -->
 					<li>
@@ -229,4 +275,3 @@
 		</div> <!-- end row -->
 	</div>
 <!-- end main content -->
->>>>>>> parent of 8f67951... adding header footer and sourcing css js
